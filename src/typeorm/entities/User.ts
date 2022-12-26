bdfcc3e35ed7073b68ecaa250d37148b00360dbe
anthony <anthony.ashraf.7@gmail.com>
2022-12-26 16:00:47 +0500

    nest js Basics

---
+++ src/typeorm/entities/User.ts
@@ -0,0 +1,31 @@
+import {Entity,PrimaryGeneratedColumn,Column, OneToOne, JoinColumn, OneToMany} from "typeorm";
+import { Post } from "./Post";
+import { Profile } from "./Profiles";
+
+@Entity({name: "users"})
+export class User{
+
+    @PrimaryGeneratedColumn({ type:"bigint" })
+    id: number;
+
+    @Column({unique: true})
+    username: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    createdAt: Date;
+
+    @Column({nullable: true})
+    authStrategy: string;
+
+    @OneToOne(() => Profile)
+    @JoinColumn()
+    profile: Profile;
+
+    @OneToMany(() => Post, (post) => post.user)
+    posts: Post[];
+
+
+}
---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Get, Post, Body, Put,Param,Delete } from '@nestjs/common';
+import { ParseIntPipe } from '@nestjs/common/pipes';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { CreateUserPostDto } from 'src/users/dtos/CreateUserPost.dto';
+import { CreateUserProfileDto } from 'src/users/dtos/CreateUserProfile.dto';
+import { UpdateUserDto } from 'src/users/dtos/updateUser.dto';
+import { UsersService } from 'src/users/services/users/users.service';
+
+@Controller('users')
+export class UsersController {
+
+    constructor(private userService: UsersService){}
+
+    @Get()
+    getUsers(){
+        return this.userService.findusers();
+    }
+
+    @Post()
+    createUser(@Body() createUserDto: CreateUserDto) {
+        // const {...userDetails, confirmPassword} = createUserDto;
+        return this.userService.createUser(createUserDto);
+    }
+
+    @Put(":id")
+    async updataUserById(@Param("id", ParseIntPipe) id:number, @Body() updateUserDto:UpdateUserDto){
+        await this.userService.updateUser(id, updateUserDto);
+    }
+
+    @Delete(":id")
+    async deleteUserById(@Param("id", ParseIntPipe) id: number) {
+        await this.userService.deleteUser(id);
+    }
+
+    @Post(":id/profiles")
+    createUserProfile(@Param("id", ParseIntPipe) id:number ,@Body() createUserProfileDto: CreateUserProfileDto){
+        return this.userService.createUserProfile(id,createUserProfileDto)
+    }
+
+    @Post(":id/posts")
+    createUserPost(@Param("id", ParseIntPipe) id:number, @Body() createUserPostDto:CreateUserPostDto) {
+        return this.userService.createUserPost(id, createUserPostDto);
+    }
+}
